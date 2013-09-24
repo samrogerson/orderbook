a4fcd0f9310c8a4da4ac2c25a6b4f0dc62af3b3d
Sam Rogerson <hyp3rfunk@gmail.com>
2013-09-24 22:07:25 +0100

    update to full test

--- runTests.sh
+++ runTests.sh
@@ -1,14 +1,15 @@
 #! /bin/bash
 
 bin="pricer.x"
-#g++ -std=c++11 -march=native -O2 -o ${bin} pricer.cpp
+if [[ ! -f ${bin} ]]
+then
+    g++ -std=c++11 -march=native -O2 -o ${bin} pricer.cpp
+fi
 
-#sizes="1 200 10000"
-sizes="200"
+sizes="1 200 10000"
 
 for size in ${sizes[@]}; 
 do
-    #cat data/pricer.in | ./${bin} $size > test.out.$size
-    head -n4384 data/pricer.in | ./${bin} $size > test.out.$size
+    cat data/pricer.in | ./${bin} $size > test.out.$size
     diff test.out.$size data/pricer.out.$size
 done
