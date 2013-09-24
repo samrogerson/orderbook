b9f682397836816836367efe4b1812519646f86d
Sam Rogerson <hyp3rfunk@gmail.com>
2013-09-24 15:25:22 +0100

    update

--- pricer.cpp
+++ pricer.cpp
@@ -1,347 +1,225 @@
+/* g++ -std=c++11 -march=native -O2 -o spirit.x spirit_test.cpp */
+
+#include <boost/config/warning_disable.hpp>
+#include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/include/phoenix_core.hpp>
+#include <boost/spirit/include/phoenix_operator.hpp>
+#include <boost/spirit/include/phoenix_object.hpp>
+#include <boost/fusion/include/adapt_struct.hpp>
+#include <boost/fusion/include/io.hpp>
+#include <boost/spirit/include/qi_lit.hpp>
+#include <boost/spirit/include/qi_hold.hpp>
+//#include <boost/phoenix/object/static_cast.hpp>
+
 #include <iostream>
 #include <string>
-#include <sstream>
 #include <vector>
-#include <algorithm>
-#include <iterator>
-#include <utility>
+#include <memory>
+#include <unordered_map>
 #include <map>
 
-#include <cstdint>
-#include <cstdlib>
-#include <cmath>
 #include <ctime>
 
-struct Order;
+namespace pricer
+{
+    namespace qi = boost::spirit::qi;
+    namespace ascii = boost::spirit::ascii;
 
-typedef std::vector<std::string> OrderTokens;
-typedef std::map<std::string, Order*> BookLookup;
-typedef std::pair<std::string, Order*> BookLookupEntry;
-typedef std::map<std::string, Order> Book;
-typedef std::pair<std::string, int> Transaction;
-
-enum class MessageType : char { ADD='A', REDUCE='R' };
-enum class OrderType : char { BID='B', ASK='S', NA };
-
-// take a string and split by spaces 
-OrderTokens tokenize(std::string& line) {
-    std::istringstream iss(line);
-    OrderTokens tokens;
-    std::copy(std::istream_iterator<std::string>(iss),
-        std::istream_iterator<std::string>(),
-        std::back_inserter<OrderTokens>(tokens));
-    return tokens;
+    struct message
+    {
+        int timestamp;
+        char mtype;
+        std::string id;
+        char otype;
+        double price;
+        int quantity;
+    };
 }
 
-struct Message {
-    MessageType mtype;
-    OrderType otype;
-    int size;
-    int timestamp;
-    double price;
-    std::string ID;
+BOOST_FUSION_ADAPT_STRUCT(
+    pricer::message,
+    (int, timestamp)
+    (char, mtype)
+    (std::string, id)
+    (char, otype)
+    (double, price)
+    (int, quantity)
+)
 
-    Message(OrderTokens &tokens) : mtype(MessageType(tokens[1].c_str()[0])),
-    ID(tokens[2]), timestamp(atoi(tokens[0].c_str())) {
-        // the position of the order size changes dependign on the type
-        int size_pos = (mtype == MessageType::ADD) ? 5 : 3;
-        size = atoi(tokens[size_pos].c_str());
-        if(mtype == MessageType::ADD) {
-            otype = OrderType(tokens[3].c_str()[0]);
-            price = atof(tokens[4].c_str());
-        } else {
-            otype = OrderType::NA;
+namespace pricer
+{
+    template <typename Iterator>
+    struct message_parser : qi::grammar<Iterator, message(), ascii::space_type>
+    {
+        message_parser() : message_parser::base_type(start)
+        {
+            using qi::int_;
+            using qi::double_;
+            using qi::as_string;
+            using qi::lexeme;
+            using qi::attr;
+            using ascii::char_;
+            using qi::_val;
+
+            start %= qi::hold[int_
+                >>  char_
+                >>  as_string[lexeme[+~char_(' ')]]
+                >>  char_
+                >>  double_
+                >>  int_]
+                |
+                int_
+                >>  char_
+                >>  as_string[lexeme[+~char_(' ')]]
+                >>  attr('0')
+                >>  attr(-1)
+                >>  int_
+                ;
         }
-    }
-    
-    void print() const {
-        std::cout << timestamp << " " << char(mtype) << " " << ID << " ";
-        if(otype != OrderType::NA) {
-            std::cout << char(otype) << " " << price << " ";
-        }
-        std::cout << size << std::endl;
-    }
-};
+        qi::rule<Iterator, message(), ascii::space_type> start;
+    };
 
-struct Order {
-    OrderType type;
-    int size;
-    double price;
+    struct order {
+        char type;
+        double price;
+        int quantity;
+        std::string id;
 
-    Order(){}
-    Order(OrderType& t, int s, double& p) : type(t), size(s), price(p) {}
-    Order(const Message& m) : type(m.otype), size(m.size), price(m.price) {}
+        order(const char &t, const double &p, const int &q, const std::string &s) : type(t),
+                price(p), quantity(q), id(s) {}
+    };
 
-    Order& operator-=(const int& deduction) {
-        size -= deduction;
-    }
-    Order& operator+=(const int& addition) {
-        size += addition;
-    }
+    struct orderbook {
+        typedef std::unordered_map<std::string, std::shared_ptr<order> > Book;
+        typedef std::multimap<int, std::shared_ptr<order> > BookIndex;
 
-    int reduce(const Message& m) {
-        int reduced_by = m.size;
-        if(m.size > size) reduced_by = size;
-        size -= m.size;
-        return reduced_by;
-    }
-
-    void print() const {
-        std::cout << "[" << char(type) <<  " " << size << " " << price <<
-            "]" << std::endl;
-    }
-};
-
-struct LookupCompare {
-        // predicate function: sort iterable of pairs of <ID, Order> by
-        // increasing price.
-        bool operator()(const BookLookupEntry &lhs, const BookLookupEntry &rhs){
-            return lhs.second->price < rhs.second->price;
-        }
-};
-
-
-struct OrderBook {
-    private:
-        // require this to be private because "asks" and "bids" reference
-        // memory owned by orders.  Therefore we require that OrderBook handles
-        // the management of this memory such that the pointers in "asks" and
-        // "bids" do not become invalid
         Book orders;
-    public:
-        BookLookup asks, bids;
-        // this allows us to choose whether we look at "asks" or "bids"
-        // depedning on the order type we are considering
-        std::map<OrderType, BookLookup*> lookup_reference;
-        int total_asks, total_bids;    
+        BookIndex asks, bids;
+        double minimum_selling_price, maximum_buying_price;
 
-        OrderBook() {
-            total_asks = 0;
-            total_bids = 0;
-
-            lookup_reference[OrderType::ASK] = &asks;
-            lookup_reference[OrderType::BID] = &bids;
+        orderbook() : minimum_selling_price(0), maximum_buying_price(0) {
         }
 
-        void print() const {
-            for(auto& o : orders) {
-                o.second.print();
-            }
-        }
-            
-        bool order_exists(const std::string& ID) {
-            return orders.find(ID)!=orders.end();
-        }
-        
-        int add_order(const Message &m) {
-            if(!order_exists(m.ID)) {
-                Order o(m);
-                orders[m.ID] = o;
-                // hilarious unreadable one liner: (*l)[m.ID] = &(orders[m.ID]);
-                BookLookup * l = lookup_reference[m.otype];
-                // l is a pointer to either a book of BUYs or SELLs (see default
-                // ctor for OrderBook). 
-                // We then make the *value* of the map buy/sell [ID] be a
-                // pointer to the new record in OrderBook.orders
-                Order* o_ptr = &(orders[m.ID]);
-                l->insert({m.ID,o_ptr});
-                if(o.type == OrderType::ASK) total_asks += o.size;
-                else if(o.type == OrderType::BID) total_bids += o.size;
-            } else {
-                std::cerr << "[ERR] received ADD order for existing ID=" <<
-                    m.ID << std::endl;
-                return -1;
-            }
-            return 0;
+        void buy() {
         }
 
-        int reduce_order(const Message &m) {
-            if(order_exists(m.ID)) {
-                Book::iterator id_order = orders.find(m.ID);
-                int deduction = id_order->second.reduce(m);
-                if(id_order->second.type == OrderType::ASK) {
-                    total_asks -= deduction;
-                } else if(id_order->second.type == OrderType::BID) {
-                    total_bids -= deduction;
-                }
-                if(id_order->second.size<=0) {
-                    OrderType t = id_order->second.type;
-                    lookup_reference[t]->erase(id_order->first);
-                    orders.erase(id_order->first);
-                }
-            } else {
-                std::cerr <<
-                    "[ERR] received REDUCE order for non-existant order ID=" <<
-                    m.ID << std::endl;
-            }
-            return 0;
-        }
-        
-        bool update(const Message &m) {
-            if(m.mtype==MessageType::ADD) {
-                add_order(m);
-            }
-            if(m.mtype==MessageType::REDUCE) {
-                reduce_order(m);
-            }
-        }
-};
-
-class TransactionManager {
-    uint64_t nmessages;
-    int target_size;
-    std::istream *stream;
-    OrderBook book;
-
-    double earnings, expenditure;
-    bool have_bought, have_sold;
-    double max_buying_price, min_sale_price;
-
-    public:
-        TransactionManager(int target, std::istream &input_stream) :
-            nmessages(0), target_size(target), stream(&input_stream),
-            earnings(0), expenditure(-1), have_bought(false), have_sold(false)
-        {}
-
-        double make_sale(int time) {
-            std::vector<BookLookupEntry> bid_portfolio(book.bids.begin(),
-                    book.bids.end());
-            // copy out map into a vector of key:value pairs
-            std::sort(bid_portfolio.begin(),bid_portfolio.end(),
-                    LookupCompare());
-            int total_sold = 0;
-            double total_takings = 0;
-            std::vector<BookLookupEntry>::iterator p = bid_portfolio.end();
-            p--;
-            min_sale_price = p->second->price;
-            while(total_sold < target_size) {
-                int available = p->second->size;
-                double price = p->second->price;
-                if(price < min_sale_price) min_sale_price = price;
-                // only sell as much as we need / is available
-                int selling = std::min(available, target_size-total_sold);
-                total_sold += selling;
-                total_takings += selling * price;
-                p--;
-            }
-            double diff = fabs(total_takings - earnings);
-            have_sold = true;
-            if(diff >= 0.005) {
-                earnings = total_takings;
-                std::cout << time << " S " << earnings <<  std::endl;
-                diff=0;
-            }
-            return total_takings;
+        void sell() {
         }
 
-        double make_purchase(int time) {
-            std::vector<BookLookupEntry> ask_portfolio(book.asks.begin(),
-                    book.asks.end());
-            std::sort(ask_portfolio.begin(),ask_portfolio.end(),
-                    LookupCompare());
-            int total_bought = 0;
-            double total_price = 0;
-            std::vector<BookLookupEntry>::iterator p = ask_portfolio.begin();
-            max_buying_price = p->second->price;
-            while(total_bought < target_size) {
-                int available = p->second->size;
-                double price = p->second->price;
-                if(price > max_buying_price) max_buying_price = price;
-                // only buy as much as we need / is available
-                int buying = std::min(available, target_size-total_bought);
-                total_bought += buying;
-                total_price += buying * price;
-                p++;
-            }
-            // we care about the *accumulated* change since we last printed
-            // a price point. once we have flucated enough to make a rounding
-            // change (0.005) we know that we need to print a new value
-            double diff = fabs(total_price - expenditure);
-            have_bought = true;
-            if(diff >= 0.005) {
-                expenditure = total_price;
-                std::cout << time << " B " << expenditure << std::endl;
-                diff=0;
-            }
-            return total_price;
+        bool remove_order(Book::iterator &o) {
+
         }
 
-        void update_positions(const Message &m) { //int time, OrderType o) {
-            // we want to reset if we've bought/sold before and the market no
-            // longer supports our target_size
-            if(have_bought && book.total_asks < target_size) {
-                std::cout << m.timestamp << " B NA" <<  std::endl;
-                expenditure = -1;
-                have_bought = false;
-            } 
-            if(have_sold && book.total_bids < target_size) {
-                std::cout << m.timestamp << " S NA" <<  std::endl;
-                earnings = 0;
-                have_sold = false;
-            }
-            // if there is sufficienty supply/demand in the mark buy/sell
-            if(book.total_asks >= target_size && m.otype!=OrderType::BID) {
-                bool impact = (m.price < max_buying_price) || m.mtype==MessageType::REDUCE;
-                if(!have_bought || impact)
-                    make_purchase(m.timestamp);
-            }
-            if(book.total_bids >= target_size && m.otype!=OrderType::ASK) {
-                bool impact = (m.price > min_sale_price) ||  m.mtype==MessageType::REDUCE;
-                if(!have_sold || impact) 
-                    make_sale(m.timestamp);
-            }
-        }
-
-        int process() {
-            std::string line;
-            while(std::getline(*stream,line)) {
-                nmessages++;
-                OrderTokens tokens = tokenize(line);
-                // check our line contains sufficient data to form a message
-                if(tokens.size() >= 4 ) { 
-                    Message m(tokens);
-                    // does our message correspond to actual stock?
-                    // strol is more robust for non-string cases but is slower
-                    // given the error checking here atoi/l is sufficient
-                    if(m.size > 0) {
-                        book.update(m);
-                        update_positions(m);
-                    } else {
-                        std::cerr << "[ERR] malformed message: size 0" <<
-                            std::endl;
-                    }
+        bool process_message(const message &m) {
+            bool do_buy(false), do_sell(false);
+            if(m.mtype=='A') {
+                // processing addition 
+                auto o = std::make_shared<order>(m.otype,m.price,m.quantity,m.id);
+                orders.insert({m.id,o});
+                if(m.otype=='A') {
+                    asks.insert({static_cast<int>(m.price*100),o});
+                    do_buy |= m.price < maximum_buying_price && maximum_buying_price > 0;
                 } else {
-                    std::cerr << "[ERR] malformed message in line: skipping" <<
-                        std::endl;
+                    bids.insert({static_cast<int>(m.price*100),o});
+                    do_sell |= m.price > minimum_selling_price && minimum_selling_price > 0;
+                }
+            } else {
+                Book::iterator o = orders.find(m.id);
+                double price = o->second->price;
+                int prev_quantity = o->second->quantity;
+                if(o->second->quantity <= m.quantity) { // need to remove it from teh indices
+                    // fully remove
+                    int k = static_cast<int>(o->second->price*100);
+                    BookIndex *index = (o->second->type == 'A') ? &asks : &bids;
+                    auto match = index->equal_range(k);
+                    for(auto it=match.first; it!= match.second; ++it) { // remove matching
+                        if(it->second->id == m.id) {
+                            index->erase(it);
+                            break;
+                        }
+                    }
+                    do_buy |= o->second->type =='A' && price < maximum_buying_price;
+                    do_sell |= o->second->type =='B' && price > minimum_selling_price;
+                    orders.erase(o);
+                } else {
+                    o->second->quantity -= m.quantity;
+                    do_buy |= o->second->type =='A' && price < maximum_buying_price;
+                    do_sell |= o->second->type =='B' && price > minimum_selling_price;
                 }
             }
-            return nmessages;
+            if(do_buy) buy();
+            if(do_sell) sell();
+            return true;
         }
 
-};
+        int process_messages(const std::vector<message> &messages) {
+            int nmess = 0;
+            for(const auto &m: messages) {
+                nmess += process_message(m);
+            }
+            return nmess;
+        }
+    };
+}
 
-int main(int argc, char** argv) {
-    // Check that we have an argument we can try to handle as an input
-    if(argc < 2) {
-        std::cout << "This program takes 1 argument" << std::endl;
-        return -1;
+unsigned int
+fetch_messages(std::vector<pricer::message>* messages, int nmessages=2e6) {
+    using boost::spirit::ascii::space;
+    typedef std::string::const_iterator iterator_type;
+    typedef pricer::message_parser<iterator_type> message_parser;
+
+    message_parser g;
+
+    messages->reserve(nmessages);
+    std::string str;
+    str.reserve(32);
+
+    unsigned int parsed_messages(0);
+    std::ios_base::sync_with_stdio (false);
+    while (getline(std::cin, str))
+    {
+        pricer::message message;
+        std::string::const_iterator iter = str.begin();
+        std::string::const_iterator end = str.end();
+        bool r = phrase_parse(iter, end, g, space, message);
+        messages->push_back(message);
+        
+        if (r && iter == end) {
+            parsed_messages++;
+        } else {
+            std::cerr << "Failed to parse line: " << str << std::endl;
+        }
     }
-    int target_size = atoi(argv[1]);
+    return parsed_messages;
+}
 
-    std::cout << std::fixed;
-    std::cout.precision(2);
+int
+main()
+{
+    std::vector<pricer::message> messages;
 
-    TransactionManager t(target_size,std::cin);
-    
     clock_t start, end;
+    double total_time;
+
+    pricer::orderbook book;
     start=clock();
-    uint64_t nmessages = t.process();
+    fetch_messages(&messages);
+    unsigned int nlines = book.process_messages(messages);
     end=clock();
 
-    double total_time = (double)(end-start)/CLOCKS_PER_SEC;
-    std::cout << "processed " << nmessages << " in " << total_time <<
-        " seconds" << std::endl;
-    std::cout << (total_time/double(nmessages))*1000 << "ms/record" << std::endl;
-    std::cout << (nmessages / total_time)/1000. << " records/ms" << std::endl;
+    total_time = (double)(end-start)/CLOCKS_PER_SEC;
+
+    std::cout << "Parsed " << nlines << " lines successfully" << std::endl;
+    std::cout << "Took " << total_time << " seconds" << std::endl;
+    std::cout << (double)(nlines / total_time) << " lines per second" << std::endl;
+
+    std::cout << "Final Line"  <<  std::endl <<
+                 "----------"  <<  std::endl <<
+                 "Timestamp: " <<  messages.back().timestamp << std::endl <<
+                 "mtype:     " <<  messages.back().mtype     << std::endl <<
+                 "id:        " <<  messages.back().id        << std::endl <<
+                 "otype:     " <<  messages.back().otype     << std::endl <<
+                 "quantity:  " <<  messages.back().quantity  << std::endl;
+
     return 0;
 }
