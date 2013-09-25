2c4540bd6350cb5c9089631858499d34a2e58a65
Sam Rogerson <hyp3rfunk@gmail.com>
2013-09-25 01:31:47 +0100

    tidy up and finalise resting

--- runTests.sh
+++ runTests.sh
@@ -3,13 +3,32 @@
 bin="pricer.x"
 if [[ ! -f ${bin} ]]
 then
+    echo -n "Compiling... "
     g++ -std=c++11 -march=native -O2 -o ${bin} pricer.cpp
+    echo "Done."
+fi
+
+prof="pricer_prof.x"
+if [[ ! -f ${prof} ]]
+then
+    g++ -pg -std=c++11 -march=native -O2 -o ${prof} pricer.cpp
 fi
 
 sizes="1 200 10000"
+data="data/pricer.in"
 
+echo "---------------------------"
+echo "Testing against sample data"
+echo "---------------------------"
 for size in ${sizes[@]}; 
 do
-    cat data/pricer.in | ./${bin} $size > test.out.$size
+    cat ${data} | ./${bin} $size > test.out.$size
     diff test.out.$size data/pricer.out.$size
 done
+echo "---------------------------"
+echo
+
+echo -n "Profiling... "
+cat ${data} | ./${prof} 10000 > test.out.10000.prof
+gprof ./pricer_prof.x gmon.out > analysis.txt
+echo "Done."
